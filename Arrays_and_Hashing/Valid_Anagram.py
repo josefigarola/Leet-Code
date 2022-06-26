1c84cb55b47f9a0a75d41c2d43c3007b39a01925
Miguel Figarola <59617503+josefigarola@users.noreply.github.com>
2022-06-26 17:59:51 -0500

    Add files via upload

---
+++ Arrays_and_Hashing/Valid_Anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        # Check both lens
+        if(len(s) != len(t)):
+            return False
+        
+        # Sort the strings
+        if(sorted(s) == sorted(t)):
+            return True
+        
