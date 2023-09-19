a6e8c94dea5f46d62fc3e6995659f3347ce2bab4
josefigarola <jmfigarolaprado2@gmail.com>
2023-09-19 16:14:54 -0600

    34 done

--- 1to100/21to40/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 1to100/21to40/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -5,43 +5,44 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        first = -1
+        second = -1
+
+        # Test case, empty array
+        if(len(nums) == 0):
+            return [first,second]
 
         # Find the first value
         p1 = 0
         p2 = len(nums)-1
-        first = 0
+        first = -1
         mid = p1 + (p2 - p1) // 2
 
-        # Test case, empty array
-        if(len(nums) == 0):
-            return [-1,-1]
-
-        while p1 <= p2:
+        while(p1 <= p2):
             mid = p1 + (p2 - p1) // 2
-            if nums[mid] >= target:
+            if(nums[mid] >= target):
                 p2 = mid - 1
             else:
                 p1 = mid + 1
             
-            if nums[mid] == target:
+            if(nums[mid] == target):
                 first = mid
-        
-        # add the target if it exists
-        if(nums[mid] != target):
-            return [-1,-1]
+
+        if(first == -1):
+            return [-1, -1]
                 
         # Find the second value
         p1 = 0
         p2 = len(nums)-1
-        second = 0
-        while p1 <= p2:
+        second = -1
+        while(p1 <= p2):
             mid = p1 + (p2 - p1) // 2
-            if nums[mid] <= target:
+            if(nums[mid] <= target):
                 p1 = mid + 1
             else:
                 p2 = mid - 1
             
-            if nums[mid] == target:
+            if(nums[mid] == target):
                 second = mid
 
         return [first,second]
